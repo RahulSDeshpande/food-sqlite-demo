7e94b67c0aa0e12819d0477ae278f0ff3a0844e6
AndroCurious <rahul.deshpande92@gmail.com>
2019-07-31 11:45:05 +0530

    SQLite Demo app
    - Implemented SQLite DB
    - Example shows how to save, update, delete & retrieve the food menu items for a restaurant

---
+++ app/src/main/java/com/rahulografy/sqlitedemo/SQLiteHelper.java
@@ -0,0 +1,75 @@
+package com.rahulografy.sqlitedemo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    SQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    void initFoodTable(String sql) {
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
+    }
+
+    void addFood(String name, String price, byte[] image) {
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "INSERT INTO FOOD VALUES (NULL, ?, ?, ?)";
+
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+
+        statement.bindString(1, name);
+        statement.bindString(2, price);
+        statement.bindBlob(3, image);
+
+        statement.executeInsert();
+    }
+
+    void updateFood(String name, String price, byte[] image, int id) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "UPDATE FOOD SET name = ?, price = ?, image = ? WHERE id = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+
+        statement.bindString(1, name);
+        statement.bindString(2, price);
+        statement.bindBlob(3, image);
+        statement.bindDouble(4, (double) id);
+
+        statement.execute();
+        database.close();
+    }
+
+    void deleteFood(int id) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "DELETE FROM FOOD WHERE id = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+        statement.bindDouble(1, (double) id);
+
+        statement.execute();
+        database.close();
+    }
+
+    Cursor getFood(String sql) {
+        SQLiteDatabase database = getReadableDatabase();
+        return database.rawQuery(sql, null);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
